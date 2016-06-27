b344bb27282e9ef98d0bf7880248483e87ba567b
Florian Meyer <florian.meyer@project-engineers.de>
2016-06-27 17:22:10 +0200

    Added solarium

--- Services/SearchService.php
+++ Services/SearchService.php
@@ -13,8 +13,57 @@ namespace StingerSoft\SolrEntitySearchBundle\Services;
 
 use StingerSoft\EntitySearchBundle\Services\AbstractSearchService;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use StingerSoft\EntitySearchBundle\Model\Query;
 
 class SearchService extends AbstractSearchService {
-
+	
 	use ContainerAwareTrait;
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \StingerSoft\EntitySearchBundle\Services\SearchService::clearIndex()
+	 */
+	public function clearIndex() {
+	}
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \StingerSoft\EntitySearchBundle\Services\SearchService::saveDocument()
+	 */
+	public function saveDocument(\StingerSoft\EntitySearchBundle\Model\Document $document) {
+	}
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \StingerSoft\EntitySearchBundle\Services\SearchService::removeDocument()
+	 */
+	public function removeDocument(\StingerSoft\EntitySearchBundle\Model\Document $document) {
+	}
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \StingerSoft\EntitySearchBundle\Services\SearchService::autocomplete()
+	 */
+	public function autocomplete($search, $maxResults = 10) {
+	}
+
+	/**
+	 *
+	 * {@inheritDoc}
+	 *
+	 * @see \StingerSoft\EntitySearchBundle\Services\SearchService::search()
+	 */
+	public function search(Query $query) {
+	}
+
+	public function getIndexSize() {
+	}
 }
